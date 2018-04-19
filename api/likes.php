dea7153ad396882a44435733618c21170d8763dd
aliastim <33629825+aliastim@users.noreply.github.com>
2018-04-19 15:16:44 +0200

    likes modif

--- api/likes.php
+++ api/likes.php
@@ -5,3 +5,30 @@
  * Date: 18/04/2018
  * Time: 15:37
  */
+session_start();
+require('../config/config.php');
+require('../model/functions.likes.php');
+
+
+//$arr = readMusics($db);
+
+
+$music_id = $_GET['like_id'];
+//$_GET['id']= 1;
+
+$data = selectMusic($db, $music_id);
+
+
+
+//echo json_encode($value);
+
+//echo json_encode($arr);
+
+header('Content-Type: application/json');
+echo json_encode($data);
+
+//foreach ($arr as &$value) {
+
+//echo $value['id'];
+//echo json_encode($value);
+//}
